fcc5b09de8bba2b9f19f07f0113ec3daaf821c18
Tales Mariano <Tales.Mariano1@gmail.com>
2024-02-27 09:25:25 -0500

    Add Animation script, prefab and new hat

---
+++ Clothes-Shop-Interview/Assets/_Scripts/CharacterSpriteAnimation.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterSpriteAnimation : MonoBehaviour
+{
+
+    public PlayerMovement character;
+    private Animator anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var direction = character.Direction;
+        var movement = character.Movement;
+
+        anim.SetFloat("Dir H", direction.x);
+        anim.SetFloat("Dir V", direction.y);
+
+        anim.SetFloat("Horizontal", movement.x);
+        anim.SetFloat("Vertical", movement.y);
+        anim.SetFloat("Speed", movement.sqrMagnitude);
+    }
+}
