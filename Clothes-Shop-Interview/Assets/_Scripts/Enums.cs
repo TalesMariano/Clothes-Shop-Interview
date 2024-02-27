00878e29b979766e6ea8d95fe07fa9d460662c97
Tales Mariano <Tales.Mariano1@gmail.com>
2024-02-27 11:12:38 -0500

    Add Clothes Sprites, Prefabs, and scriptable objecs

---
+++ Clothes-Shop-Interview/Assets/_Scripts/Enums.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enums : MonoBehaviour
+{
+
+}
+
+public enum ClothesPart
+{
+    None,
+    Underwear,
+    Body,
+    Head
+}
